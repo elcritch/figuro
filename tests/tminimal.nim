28720ef03850a3ed637a89e9478f632724fdb254
Jaremy Creechley <creechley@gmail.com>
2023-07-30 22:48:40 -0700

    add test

---
+++ tests/tminimal.nim
@@ -0,0 +1,14 @@
+
+## This minimal example shows 5 blue squares.
+
+import figura/engine
+
+proc drawMain() =
+  frame "main":
+    box 0, 0, 620, 140
+    for i in 0 .. 4:
+      group "block":
+        box 20 + i * 120, 20, 100, 100
+        fill "#2B9FEA"
+
+startFidget(drawMain, w = 620, h = 140)
