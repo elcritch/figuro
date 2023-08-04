61512e63411fe8f910973ced944cc66c1bb890cf
Jaremy Creechley <creechley@gmail.com>
2023-08-03 21:28:19 -0700

    new nodes file

--- figuro/widgets/apis.nim
+++ figuro/widgets/apis.nim
@@ -13,142 +13,12 @@ import cdecl/atoms
 from windy/common import Button, ButtonView
 
 import ../[common, commonutils]
+import ./nodes
 
-export chroma, common
-export commonutils
-export cssgrid
+export chroma, cssgrid
+export commonutils, common, nodes
 
 import pretty
-
-var
-  parent*: Node
-  root*: Node
-
-  nodeStack*: seq[Node]
-  gridStack*: seq[GridTemplate]
-  current*: Node
-  scrollBox*: Box
-  scrollBoxMega*: Box ## Scroll box is 500px bigger in y direction
-  scrollBoxMini*: Box ## Scroll box is smaller by 100px useful for debugging
-  numNodes*: int
-  popupActive*: bool
-  inPopup*: bool
-  resetNodes*: int
-  popupBox*: Box
-
-  # Used to check for duplicate ID paths.
-  pathChecker*: Table[string, bool]
-
-  computeTextLayout*: proc(node: Node)
-
-  lastUId: int
-  nodeLookup*: Table[string, Node]
-
-  ## Used for HttpCalls
-  httpCalls*: Table[string, HttpCall]
-
-  defaultlineHeightRatio* = 1.618.UICoord ##\
-    ## see https://medium.com/@zkareemz/golden-ratio-62b3b6d4282a
-  adjustTopTextFactor* = 1/16.0 # adjust top of text box for visual balance with descender's -- about 1/8 of fonts, so 1/2 that
-
-  # global scroll bar settings
-  scrollBarFill* = rgba(187, 187, 187, 162).color 
-  scrollBarHighlight* = rgba(137, 137, 137, 162).color
-
-  buttonPress: ButtonView
-  buttonDown: ButtonView
-  buttonRelease: ButtonView
-
-
-inputs.keyboardInput = proc (rune: Rune) =
-    requestedFrame.inc
-    # if keyboard.focusNode != nil:
-    #   keyboard.state = KeyState.Press
-    #   # currTextBox.typeCharacter(rune)
-    # else:
-    #   keyboard.state = KeyState.Press
-    #   keyboard.keyString = rune.toUTF8()
-    uiEvent.trigger()
-
-proc newUId*(): NodeUID =
-  # Returns next numerical unique id.
-  inc lastUId
-  when defined(js) or defined(StringUID):
-    $lastUId
-  else:
-    NodeUID(lastUId)
-
-proc refresh*() =
-  ## Request the screen be redrawn
-  requestedFrame = max(1, requestedFrame)
-
-proc preNode(kind: NodeKind, id: Atom) =
-  ## Process the start of the node.
-
-  parent = nodeStack[^1]
-
-  # TODO: maybe a better node differ?
-  if parent.nodes.len <= parent.diffIndex:
-    # Create Node.
-    current = Node()
-    current.uid = newUId()
-    parent.nodes.add(current)
-    refresh()
-  else:
-    # Reuse Node.
-    current = parent.nodes[parent.diffIndex]
-    if resetNodes == 0 and
-        current.nIndex == parent.diffIndex:
-      # Same node.
-      discard
-    else:
-      # Big change.
-      current.nIndex = parent.diffIndex
-      # current.resetToDefault()
-      refresh()
-
-  current.kind = kind
-  # current.textStyle = parent.textStyle
-  # current.cursorColor = parent.cursorColor
-  current.highlight = parent.highlight
-  current.transparency = parent.transparency
-  current.zlevel = parent.zlevel
-  nodeStack.add(current)
-  inc parent.diffIndex
-
-  current.diffIndex = 0
-
-proc postNode() =
-  current.removeExtraChildren()
-
-  # Pop the stack.
-  discard nodeStack.pop()
-  if nodeStack.len > 1:
-    current = nodeStack[^1]
-  else:
-    current = nil
-  if nodeStack.len > 2:
-    parent = nodeStack[^2]
-  else:
-    parent = nil
-
-template node(kind: NodeKind, id: static string, inner, setup: untyped): untyped =
-  ## Base template for node, frame, rectangle...
-  preNode(kind, atom(id))
-  setup
-  inner
-  postNode()
-
-template node(kind: NodeKind, id: static string, inner: untyped): untyped =
-  ## Base template for node, frame, rectangle...
-  preNode(kind, atom(id))
-  inner
-  postNode()
-
-
-template withDefaultName(name: untyped): untyped =
-  template `name`*(inner: untyped): untyped =
-    `name`("", inner)
 
 
 proc defaultLineHeight*(fontSize: UICoord): UICoord =
@@ -197,6 +67,10 @@ mouse.pos = vec2(0, 0)
 ## Fidget nodes. 
 ## 
 
+proc boxFrom*(x, y, w, h: float32) =
+  ## Sets the box dimensions.
+  current.box = initBox(x, y, w, h)
+
 template frame*(id: static string, inner: untyped): untyped =
   ## Starts a new frame.
   node(nkFrame, id, inner):
@@ -222,14 +96,14 @@ template rectangle*(id, inner: untyped): untyped =
 ## 
 ## Various overloaded node APIs
 
-withDefaultName(group)
+template withDefaultName(name: untyped): untyped =
+  template `name`*(inner: untyped): untyped =
+    `name`("", inner)
+
 withDefaultName(frame)
 withDefaultName(rectangle)
 withDefaultName(text)
-withDefaultName(component)
-withDefaultName(instance)
 withDefaultName(drawable)
-withDefaultName(blank)
 
 template rectangle*(color: string|Color) =
   ## Shorthand for rectangle with fill.
@@ -262,10 +136,6 @@ proc clearInputs*() =
 ## These APIs provide the basic functionality for
 ## interacting with user interactions. 
 ## 
-
-proc boxFrom(x, y, w, h: float32) =
-  ## Sets the box dimensions.
-  current.box = initBox(x, y, w, h)
 
 proc fltOrZero(x: int|float32|float64|UICoord|Constraint): float32 =
   when x is Constraint:
