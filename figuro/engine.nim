1756a04ce62845e4e946b494956ccbaa61e35dab
Jaremy Creechley <creechley@gmail.com>
2023-08-05 02:49:07 -0700

    child nodes

--- figuro/engine.nim
+++ figuro/engine.nim
@@ -43,7 +43,6 @@ else:
         appNodes.setupRoot()
         appMain()
         computeScreenBox(nil, appNodes)
-        echo "run App"
         sendRoot(appNodes.copyInto())
         os.sleep(16)
 
