898daee942f5a0c1994633289a1527f5b70e12e3
Jaremy Creechley <creechley@gmail.com>
2023-08-06 03:55:18 -0700

    tests

--- figuro/meta/slots.nim
+++ figuro/meta/slots.nim
@@ -179,7 +179,7 @@ macro rpcImpl*(p: untyped, publish: untyped, qarg: untyped): untyped =
     echo result.repr
 
   elif isSignal:
-    var construct = nnkTupleConstr(parameters[1..^1])
+    var construct = nnkTupleConstr.newTree(parameters[1..^1])
     echo "const: ", construct.repr
 
     result.add quote do:
