0de4de39212b1ca47403e0d21d69058860e160af
Jaremy Creechley <creechley@gmail.com>
2023-08-05 18:56:59 -0700

    add threading / orc reqs

--- figuro.nim
+++ figuro.nim
@@ -5,8 +5,3 @@ import figuro/engine
 
 export shared, apis, engine
 
-when not compileOption("threads"):
-  {.error: "This module requires --threads:on compilation flag".}
-
-when not defined(gcArc) and not defined(gcOrc) and not defined(nimdoc):
-  {.error: "This channel implementation requires --gc:arc or --gc:orc".}
