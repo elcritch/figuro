76397170e4960a61bc754d189ac9953c7e626c60
Jaremy Creechley <creechley@gmail.com>
2023-08-09 22:52:18 -0700

    cleanup

--- tests/tminimal.nim
+++ tests/tminimal.nim
@@ -1,10 +1,14 @@
 
 ## This minimal example shows 5 blue squares.
 
-import figuro/timers
+import figuro/[timers, widget]
 import figuro
 
-proc drawMain() =
+type
+  Main* = ref object of Figuro
+    value: float
+
+method render(app: Main) =
   frame "main":
     box 0, 0, 620, 140
     for i in 0 .. 4:
@@ -12,4 +16,4 @@ proc drawMain() =
         box 20 + i * 120, 20, 100, 100
         current.fill = parseHtmlColor "#2B9FEA"
 
-startFiguro(drawMain, w = 620, h = 140)
+startFiguro(Main(), w = 620, h = 140)
