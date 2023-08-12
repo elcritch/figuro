1428afa8de5884946c1e645029252d7fb03b7716
Jaremy Creechley <creechley@gmail.com>
2023-08-11 18:08:13 -0700

    fix twidget

--- figuro/widgets/button.nim
+++ figuro/widgets/button.nim
@@ -28,7 +28,7 @@ proc draw*(self: Button) {.slot.} =
     fill "#FF0000"
   else:
     fill "#2B9FEA"
-    onHover:
-      fill "#00FF00"
+    # onHover:
+    #   fill "#00FF00"
     # onClick:
     #   fill "#00FFFF"
