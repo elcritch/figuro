87e499f1c975b035d585bace61cfa48e20aa76f1
Jaremy Creechley <creechley@gmail.com>
2023-08-25 18:59:50 -0700

    add pretty hover

--- tests/tminimal.nim
+++ tests/tminimal.nim
@@ -8,21 +8,33 @@ type
   Main* = ref object of Figuro
     value: float
     hasHovered: bool
+    hoveredAlpha: float
     mainRect: Figuro
 
 proc hover*(self: Main, kind: EventKind) {.slot.} =
   self.hasHovered = kind == Enter
+
   # echo "main: child hovered: ", kind
   refresh(self)
+
+proc tick*(self: Main) {.slot.} =
+  if self.hasHovered:
+    if self.hoveredAlpha < 0.2:
+      refresh(self)
+      self.hoveredAlpha += 0.004
+  else:
+    if self.hoveredAlpha > 0.01:
+      self.hoveredAlpha -= 0.004
+      refresh(self)
+    # self.hoveredAlpha = clamp(self.value mod 1.0, 0, 1.0)
 
 proc draw*(self: Main) {.slot.} =
   rectangle "main":
     self.mainRect = current
     box 10, 10, 600, 120
     cornerRadius 10.0
-    fill whiteColor
-    if self.hasHovered:
-      fill whiteColor.darken(0.12)
+    # fill whiteColor
+    fill whiteColor.darken(self.hoveredAlpha).spin(10*self.hoveredAlpha)
     for i in 0 .. 4:
       button "btn":
         box 10 + i * 120, 10, 100, 100
@@ -34,13 +46,14 @@ proc draw*(self: Main) {.slot.} =
         # us
 
 var
-  fig = Main.new()
+  main = Main.new()
 
-connect(fig, onDraw, main, Main.draw)
+connect(main, onDraw, main, Main.draw)
+connect(main, onTick, main, Main.tick)
 
 echo "main: ", main.listeners
 
 app.width = 720
 app.height = 140
 
-startFiguro(fig)
+startFiguro(main)
