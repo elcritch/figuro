0d72fbd458b82768a8f207a09fec716870ff0fa6
Jaremy Creechley <creechley@gmail.com>
2023-09-02 01:03:40 -0700

    fixup slot types

--- figuro/widgets/button.nim
+++ figuro/widgets/button.nim
@@ -72,7 +72,7 @@ template button*[T; V](typ: typedesc[T], name: string, value: V, blk: untyped) =
     # connect(current, onDraw, current, Button[T].draw())
     connect(current, onDraw, current, postDraw)
     connect(current, onClick, current, Button[T].clicked)
-    connect(current, onHover, current, Button[T].hovered)
+    # connect(current, onHover, current, Button[T].hovered)
     postNode(current, parent)
 
 template button*[V](id: string, value: V, blk: untyped) =
