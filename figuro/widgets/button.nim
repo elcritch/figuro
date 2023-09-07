1a0b2571fd51a2bf932b0e3c1aa22e04a1ee7851
Jaremy Creechley <creechley@gmail.com>
2023-09-06 21:09:57 -0700

    nodes

--- figuro/widgets/button.nim
+++ figuro/widgets/button.nim
@@ -54,20 +54,41 @@ type
 
 proc state*[T](tp: typedesc[T]): State[T] = discard
 
-template buttonWidget*[T](
-                          name: string,
-                          blk: untyped) =
-  block:
-    var parent: Figuro = Figuro(current)
-    var current {.inject.}: Button[T] = nil
+# template buttonWidget*[T](
+#                           name: string,
+#                           blk: untyped) =
+#   block:
+#     var parent: Figuro = Figuro(current)
+#     var current {.inject.}: Button[T] = nil
+#     preNode(nkRectangle, name, current, parent)
+#     captureArgs value:
+#       current.postDraw = proc (widget: Figuro) =
+#         var current {.inject.}: Button[T] = Button[T](widget)
+#         if postDrawReady in widget.attrs:
+#           widget.attrs.excl postDrawReady
+#           `blk`
+#     postNode(Figuro(current))
+
+proc buttonWidget*[T](
+    name: string,
+    current: var Button[T],
+    parent: var Figuro,
+    body: proc (self: Button[T])
+) =
     preNode(nkRectangle, name, current, parent)
-    captureArgs value:
-      current.postDraw = proc (widget: Figuro) =
-        var current {.inject.}: Button[T] = Button[T](widget)
-        if postDrawReady in widget.attrs:
-          widget.attrs.excl postDrawReady
-          `blk`
+    current.postDraw = proc (widget: Figuro) =
+        var self: Button[T] = Button[T](widget)
+        if postDrawReady in self.attrs:
+          self.attrs.excl postDrawReady
+          body(self)
     postNode(Figuro(current))
+
+template button*[T](name: string,
+                body: proc (self: Button[T])) =
+  block:
+    var parent: Figuro = current
+    var current {.inject.}: Button[T] = nil
+    buttonWidget(name, current, parent, body)
 
 # template button*[T](s: State[T] = state(void),
 #                     name: string,
@@ -85,62 +106,62 @@ template buttonWidget*[T](
 
 from sugar import capture
 import macros
-macro button*(args: varargs[untyped]) =
-  echo "button:\n", args.treeRepr
-  # echo "do:\n", args[2].treeRepr
-  let id = args[0]
-  var stateArg: NimNode
-  var blk: NimNode
-  var capturedVals: NimNode
-  var isCaptured: bool
+# macro button*(args: varargs[untyped]) =
+#   echo "button:\n", args.treeRepr
+#   # echo "do:\n", args[2].treeRepr
+#   let id = args[0]
+#   var stateArg: NimNode
+#   var blk: NimNode
+#   var capturedVals: NimNode
+#   var isCaptured: bool
 
-  for arg in args:
-    if arg.kind == nnkExprEqExpr and arg[0].repr == "state":
-      arg[1].expectKind(nnkBracket)
-      assert arg[1].len() == 1
-      stateArg = arg[1][0]
+#   for arg in args:
+#     if arg.kind == nnkExprEqExpr and arg[0].repr == "state":
+#       arg[1].expectKind(nnkBracket)
+#       assert arg[1].len() == 1
+#       stateArg = arg[1][0]
 
-  if args[2].kind == nnkDo:
-    let doblk = args[2]
-    let pms = doblk.params()
-    blk = doblk[^1]
-    echo "pms:\n", pms.treeRepr
+#   if args[2].kind == nnkDo:
+#     let doblk = args[2]
+#     let pms = doblk.params()
+#     blk = doblk[^1]
+#     echo "pms:\n", pms.treeRepr
 
-    var vals = nnkBracket.newNimNode()
-    for arg in pms[1..^1]: vals.add arg[0]
-    capturedVals = vals
-    echo "vals: ", vals.repr
-    echo "state: ", stateArg.repr
-    isCaptured = true
-  else:
-    capturedVals = nnkBracket.newTree()
-    blk = args[^1]
+#     var vals = nnkBracket.newNimNode()
+#     for arg in pms[1..^1]: vals.add arg[0]
+#     capturedVals = vals
+#     echo "vals: ", vals.repr
+#     echo "state: ", stateArg.repr
+#     isCaptured = true
+#   else:
+#     capturedVals = nnkBracket.newTree()
+#     blk = args[^1]
 
-  let body = quote do:
-      current.postDraw = proc (widget: Figuro) =
-        var current {.inject.}: Button[`stateArg`] = Button[`stateArg`](widget)
-        if postDrawReady in widget.attrs:
-          widget.attrs.excl postDrawReady
-          `blk`
+#   let body = quote do:
+#       current.postDraw = proc (widget: Figuro) =
+#         var current {.inject.}: Button[`stateArg`] = Button[`stateArg`](widget)
+#         if postDrawReady in widget.attrs:
+#           widget.attrs.excl postDrawReady
+#           `blk`
 
-  let outer = 
-    if isCaptured:
-      quote do:
-        capture `capturedVals`:
-          `body`
-    else:
-      quote do:
-        `body`
+#   let outer = 
+#     if isCaptured:
+#       quote do:
+#         capture `capturedVals`:
+#           `body`
+#     else:
+#       quote do:
+#         `body`
 
-  result = quote do:
-    block:
-      var parent: Figuro = Figuro(current)
-      var current {.inject.}: Button[`stateArg`] = nil
-      preNode(nkRectangle, `id`, current, parent)
-      `outer`
-      postNode(Figuro(current))
+#   result = quote do:
+#     block:
+#       var parent: Figuro = Figuro(current)
+#       var current {.inject.}: Button[`stateArg`] = nil
+#       preNode(nkRectangle, `id`, current, parent)
+#       `outer`
+#       postNode(Figuro(current))
 
-  echo "button:result:\n", result.repr
+#   echo "button:result:\n", result.repr
   
 
 
