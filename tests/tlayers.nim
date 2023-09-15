0d9dade7df0ca58ac4d66fcf8ec695953afb87ee
Jaremy Creechley <creechley@gmail.com>
2023-09-15 02:59:55 -0700

    fails zlvl 10

--- tests/tlayers.nim
+++ tests/tlayers.nim
@@ -18,22 +18,22 @@ proc draw*(self: Main) {.slot.} =
     box 0, 0, 100'vw, 100'vh
     rectangle "body":
       fill "#D0D0D0"
-      box 10'pw, 10'ph, 80'pw, 80'ph
+      box 10'pw, 10'ph, 40'pw, 80'ph
       cornerRadius 10.0
       # clipContent true
 
       button "btn":
-        box 10'pw, 10'ph, 160'pw, 10'ph
+        box 10'pw, 10'ph, 160'pw, 20'ph
         # fill "#2B9FEA"
         current.zlevel = 20.ZLevel
         button "btn":
           # fill "#2B9FEA".parseHtmlColor.darken(0.2)
           box 10'pw, 10'ph, 80'pw, 60'ph
 
-        button "btn":
-          box 10'pw, 60'ph, 160'pw, 60'ph
-          fill "#CB0F0A"
-          current.zlevel = 15.ZLevel
+      button "btn":
+        box 10'pw, 60'ph, 160'pw, 20'ph
+        fill "#CB0F0A"
+        current.zlevel = 5.ZLevel
 
 var main = Main.new()
 connect(main, doDraw, main, Main.draw)
@@ -41,7 +41,7 @@ connect(main, doTick, main, Main.tick)
 
 echo "main: ", main.listeners
 
-app.width = 400
+app.width = 600
 app.height = 400
 
 startFiguro(main)
