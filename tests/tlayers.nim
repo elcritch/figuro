ae85875e11c192f84d6a473c7439129d4ffec013
Jaremy Creechley <creechley@gmail.com>
2023-09-16 17:07:49 -0700

    handling multiple layers

--- tests/tlayers.nim
+++ tests/tlayers.nim
@@ -15,23 +15,32 @@ type
 proc draw*(self: Main) {.slot.} =
   withDraw(self):
     box 0, 0, 100'vw, 100'vh
-    rectangle "body":
+    rectangle "container":
       fill "#D0D0D0"
-      box 10'pw, 10'ph, 40'pw, 80'ph
+      box 3'pw, 10'ph, 30'pw, 80'ph
       cornerRadius 10.0
       # clipContent true
 
       button "btn":
-        box 10'pw, 10'ph, 160'pw, 20'ph
-        # fill "#2B9FEA"
+        box 10'pw, 10'ph, 130'pw, 20'ph
         current.zlevel = 20.ZLevel
-        button "btn":
-          # fill "#2B9FEA".parseHtmlColor.darken(0.2)
-          box 10'pw, 10'ph, 80'pw, 60'ph
 
       button "btn":
-        box 10'pw, 60'ph, 160'pw, 20'ph
-        # fill "#CB0F0A"
+        box 10'pw, 60'ph, 130'pw, 20'ph
+        current.zlevel = -5.ZLevel
+
+    rectangle "container":
+      fill "#D0D0D0"
+      box 50'pw, 10'ph, 30'pw, 80'ph
+      cornerRadius 10.0
+      # clipContent true
+
+      button "btn":
+        box 10'pw, 10'ph, 130'pw, 20'ph
+        current.zlevel = 20.ZLevel
+
+      button "btn":
+        box 10'pw, 60'ph, 130'pw, 20'ph
         current.zlevel = -5.ZLevel
 
 var main = Main.new()
@@ -40,7 +49,7 @@ connect(main, doTick, main, Main.tick)
 
 echo "main: ", main.listeners
 
-app.width = 600
+app.width = 800
 app.height = 400
 
 startFiguro(main)
