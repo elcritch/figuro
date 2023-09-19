b7fdf64b1e1236ce096a667d6ab219d6dd042d79
Jaremy Creechley <creechley@gmail.com>
2023-09-18 23:07:20 -0700

    plumbing keys

--- figuro/widgets/input.nim
+++ figuro/widgets/input.nim
@@ -7,6 +7,7 @@ type
   Input* = ref object of Figuro
     isActive*: bool
     disabled*: bool
+    selection*: Slice[int]
     text*: string
 
 let
@@ -15,8 +16,8 @@ let
   smallFont = UiFont(typefaceId: typeface, size: 12'ui)
 
 proc clicked*(self: Input,
-                  kind: EventKind,
-                  buttons: UiButtonView) {.slot.} =
+              kind: EventKind,
+              buttons: UiButtonView) {.slot.} =
   echo nd(), "input:clicked: ", buttons,
               " kind: ", kind, " :: ", self.getId
 
@@ -28,8 +29,9 @@ proc clicked*(self: Input,
   refresh(self)
 
 proc keyInput*(self: Input,
-                rune: Rune) {.slot.} =
+               rune: Rune) {.slot.} =
   echo nd(), "Input:rune: ", $rune, " :: ", self.getId
+  self.selection = self.text.len() .. self.text.len()
   self.text.add($rune)
   refresh(self)
 
@@ -37,8 +39,12 @@ proc keyPress*(self: Input,
                pressed: UiButtonView,
                down: UiButtonView) {.slot.} =
   echo nd(), "Input:keyPress: ", " pressed: ", $pressed, " down: ", $down, " :: ", self.getId
-  # self.text.add($keys)
-  refresh(self)
+  if pressed == {KeyBackspace}:
+    echo "self.selection: ", self.selection, " len: ", self.text.len()
+    self.text.delete(self.selection)
+    self.selection = self.text.len() - 1 .. self.text.len() - 1
+    echo "self.selection:post: ", self.selection, " len: ", self.text.len()
+    refresh(self)
 
 proc draw*(self: Input) {.slot.} =
   ## Input widget!
