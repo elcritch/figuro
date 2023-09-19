d6fe7b66ac06873d66fae4cb74b96bf39296ab58
Jaremy Creechley <creechley@gmail.com>
2023-09-18 23:47:28 -0700

    plumbing keys

--- figuro/widgets/input.nim
+++ figuro/widgets/input.nim
@@ -9,6 +9,7 @@ type
     disabled*: bool
     selection*: Slice[int]
     text*: string
+    layout*: GlyphArrangement
 
 let
   typeface = loadTypeFace("IBMPlexSans-Regular.ttf")
@@ -55,15 +56,17 @@ proc draw*(self: Input) {.slot.} =
       box 10, 10, 400, 100
       fill blackColor
       setText({font: self.text})
+      self.layout = current.textLayout
 
       rectangle "cursor":
         # echo "cursor: ", self.selection
         # echo "cursor: ", current.parent.textLayout.repr
-        let sz = 0..current.parent.textLayout.selectionRects.high()
+        let sz = 0..self.layout.selectionRects.high()
         if self.selection.a in sz and self.selection.b in sz: 
-          var sr = current.parent.textLayout.selectionRects[self.selection.b]
+          var sr = self.layout.selectionRects[self.selection.b]
           sr.x = sr.x + sr.w
-          sr.w = 0.1 * font.size.scaled
+          sr.y = sr.y - 0.25 * font.size.scaled
+          sr.w = 0.1 * font.size.scaled * 0.7
           box sr.descaled()
           # box 0, 0, font.size*0.04, font.size
           fill blackColor
