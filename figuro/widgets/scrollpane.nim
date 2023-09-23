157b5d965e57346803e5335b9b87ad24bd123220
Jaremy Creechley <creechley@gmail.com>
2023-09-23 15:06:46 -0700

    initial scrollpane

---
+++ figuro/widgets/scrollpane.nim
@@ -0,0 +1,33 @@
+
+import commons
+import ../ui/utils
+
+type
+  ScrollPane* = ref object of Figuro
+    isActive*: bool
+    disabled*: bool
+    scrollby: Position
+    mainRect: Figuro
+
+import pretty
+
+proc scroll*(self: ScrollPane, wheelDelta: Position) {.slot.} =
+  # self.scrollby.x -= wheelDelta.x * 10.0
+  self.scrollby.y -= wheelDelta.y * 10.0
+  self.scrollby.y = self.scrollby.y.clamp(0.0, self.box.y)
+  refresh(self)
+
+proc draw*(self: ScrollPane) {.slot.} =
+  withDraw(self):
+    box 20, 10, 80'vw, 300
+    current.listens.events.incl evScroll
+    connect(current, doScroll, self, ScrollPane.scroll)
+    rectangle "body":
+      boxOf current.parent
+      cornerRadius 10.0
+      fill whiteColor.darken(0.1)
+      clipContent true
+      current.offset = self.scrollby
+      current.attrs.incl scrollPanel
+
+exportWidget(scroll, ScrollPane)
