615569e911d8e70acc106fe31692ea0149d07fc7
Jaremy Creechley <creechley@gmail.com>
2023-09-24 14:37:27 -0700

    button test

---
+++ tests/tbutton.nim
@@ -0,0 +1,47 @@
+import figuro/widgets/buttonWrap
+import figuro/widget
+import figuro
+
+let
+  typeface = loadTypeFace("IBMPlexSans-Regular.ttf")
+  font = UiFont(typefaceId: typeface, size: 22)
+
+type
+  Counter* = object
+
+  Main* = ref object of Figuro
+    value: int
+    hasHovered: bool
+    hoveredAlpha: float
+    mainRect: Figuro
+
+
+proc draw*(self: Main) {.slot.} =
+  withDraw(self):
+    self.name.setLen(0)
+    self.name.add "main"
+    fill "#9F2B00"
+    box 0, 0, 400, 300
+
+    rectangle "btnBody":
+      box 10, 10, 10, 10
+      fill "#9F2B00"
+
+    button "btn", state(int):
+      echo nd(), "button:WIDGET: ", current.getId
+      box 40, 40, 100, 100
+      fill "#2B9F2B"
+
+      # node nkText, "btnText":
+      #   echo nd(), "btnText:WIDGET: ", current.getId, " ", widget.getId
+      #   box 10'pw, 10'pw, 80'pw, 80'ph
+      #   fill blackColor
+      #   setText({font: "hi"})
+
+var main = Main.new()
+connect(main, doDraw, main, Main.draw())
+connect(main, doTick, main, Main.tick())
+
+app.width = 720
+app.height = 140
+startFiguro(main)
