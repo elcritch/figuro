4bb063d7d58f9c1deccef797b2dc9591e30a2dc4
Jaremy Creechley <creechley@gmail.com>
2023-09-26 23:29:12 -0700

    cleanup

--- tests/tbutton.nim
+++ tests/tbutton.nim
@@ -21,19 +21,19 @@ proc draw*(self: Main) {.slot.} =
     self.name.setLen(0)
     self.name.add "main"
     fill "#9F2B00"
-    box 0, 0, 400, 300
+    box 0'ux, 0'ux, 400'ux, 300'ux
 
     rectangle "btnBody":
-      box 10, 10, 10, 10
+      box 10'ux, 10'ux, 10'ux, 10'ux
       fill "#9F2B00"
 
     button "btn", state(int):
-      box 10, 10, 80'ph, 80'pw
+      box 10'ux, 10'ux, 80'pp, 80'pp
       fill "#2B9F2B"
 
       contents "child":
         node nkText, "btnText":
-          box 10'pw, 10'pw, 80'pw, 80'ph
+          box 10'pp, 10'pp, 80'pp, 80'pp
           fill blackColor
           setText({font: "hi"})
 
