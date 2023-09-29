e6de947cb13fc9710c99dd7401d76fa05a0e1468
Jaremy Creechley <creechley@gmail.com>
2023-09-29 15:20:06 -0700

    add tdrag

---
+++ tests/tdrag.nim
@@ -0,0 +1,55 @@
+import figuro/widgets/buttonWrap
+import figuro/widget
+import figuro
+
+let
+  typeface = loadTypeFace("IBMPlexSans-Regular.ttf")
+  font = UiFont(typefaceId: typeface, size: 16)
+
+type
+  Counter* = object
+
+  Main* = ref object of Figuro
+    value: int
+    hasHovered: bool
+    hoveredAlpha: float
+    mainRect: Figuro
+
+proc btnDrag*(node: Figuro,
+              kind: EventKind,
+              initial: Position,
+              cursor: Position) {.slot.} =
+  echo "btnDrag: ", node.getId, " ", kind,
+          " change: ", initial.positionDiff(cursor),
+          " nodeRel: ", cursor.positionRelative(node)
+
+proc draw*(self: Main) {.slot.} =
+  withDraw(self):
+    self.name.setLen(0)
+    self.name.add "main"
+    fill "#9F2B00"
+    box 0'ux, 0'ux, 400'ux, 300'ux
+
+    button "btn", state(int):
+      box 40'ux, 30'ux, 80'ux, 80'ux
+      fill "#2B9F2B"
+      connect(current, doDrag, current, btnDrag)
+
+      contents "child":
+        node nkText, "btnText":
+          box 10'ux, 10'ux, 80'pp, 80'pp
+          fill blackColor
+          setText({font: "drag me"})
+
+    rectangle "btnBody":
+      box 200'ux, 30'ux, 80'ux, 80'ux
+      fill "#9F2B00"
+      connect(current, doDrag, current, btnDrag)
+
+
+var main = Main.new()
+connect(main, doDraw, main, Main.draw())
+
+app.width = 400
+app.height = 140
+startFiguro(main)
