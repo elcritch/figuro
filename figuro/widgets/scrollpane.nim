7876803906c370ba4b4b9da74e7fbbbf2be1daf8
Jaremy Creechley <creechley@gmail.com>
2023-10-21 18:22:07 -0700

    setup scroll min-max

--- figuro/widgets/scrollpane.nim
+++ figuro/widgets/scrollpane.nim
@@ -7,22 +7,51 @@ type
     isActive*: bool
     disabled*: bool
     scrollby: Position
-    mainRect: Figuro
-
-import pretty
+    width*: UICoord = 20.0'ui
+    barLeft: bool
+    bar*: ScrollBar
+  
+  ScrollBar* = object
+    viewHeight: UICoord
+    contentHeight: UICoord
+    contentOverflow: UICoord
+    contentViewRatio: UICoord
+    size: UICoord
+    sizePercent: UICoord
+    start: Position
 
 proc scroll*(self: ScrollPane, wheelDelta: Position) {.slot.} =
   # self.scrollby.x -= wheelDelta.x * 10.0
   let yoffset = wheelDelta.y * 10.0
   let current = self.children[0]
   assert current.name == "scrollBody"
-
   let
     viewHeight = self.screenBox.h
-    contentOverflow = (current.screenBox.h - viewHeight).clamp(0'ui, current.screenBox.h)
-  # echo "SCROLL: ph: ", viewHeight, " ch: ", contentOverflow
+    contentHeight = current.screenBox.h
+    contentViewRatio = (viewHeight/contentHeight).clamp(0.0'ui, 1.0'ui)
+    contentOverflow = (contentHeight - viewHeight).clamp(0'ui, current.screenBox.h)
+
+  echo "SCROLL: ph: ", viewHeight, " ch: ", contentOverflow, " ratio: ", contentViewRatio
   self.scrollby.y -= yoffset
   self.scrollby.y = self.scrollby.y.clamp(0'ui, contentOverflow)
+
+  let
+    scrollBarSize = contentViewRatio * viewHeight
+    sizePercent = clamp(self.scrollby.y/contentOverflow, 0'ui, 1'ui)
+    barX = if self.barLeft: 0'ui
+                   else: self.screenBox.w - self.width
+    barY = sizePercent*(viewHeight - scrollBarSize)
+    barStart = initPosition(barX.float, barY.float)
+
+  self.bar = ScrollBar(
+    viewHeight: viewHeight,
+    contentHeight: contentHeight,
+    contentViewRatio: contentViewRatio,
+    contentOverflow: contentOverflow,
+    size: scrollBarSize,
+    sizePercent: sizePercent,
+    start: barStart,
+  )
 
   refresh(self)
 
@@ -42,6 +71,9 @@ proc draw*(self: ScrollPane) {.slot.} =
 
       # echo "SCROLL BODY: ", node.box, " => ", node.children[0].box
       # boxSizeOf node.children[0]
+    rectangle "scrollBody":
+      box self.bar.start.x, self.bar.start.y, self.width, self.bar.size
+      fill blackColor
 
 proc getWidgetParent*(self: ScrollPane): Figuro =
   # self.children[0] # "scrollBody"
