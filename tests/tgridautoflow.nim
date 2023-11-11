aa2ff4f8d8448086f79a1519ae8d88465589a715
Jaremy Creechley <creechley@gmail.com>
2023-11-11 14:15:15 -0700

    change apis

--- tests/tgridautoflow.nim
+++ tests/tgridautoflow.nim
@@ -3,9 +3,6 @@ import figuro/widgets/button
 import figuro/widget
 import figuro
 
-const hasGaps = false
-
-
 type
   GridApp = ref object of Figuro
     ## this creates a new ref object type name using the
@@ -18,46 +15,51 @@ proc draw*(self: GridApp) {.slot.} =
   nodes(self):
     rectangle "autoLayout":
       # font "IBM Plex Sans", 16, 400, 16, hLeft, vCenter
-      box 10'pp, 10'pp, 80'pp, 80'pp
-      fill rgb(224, 239, 255).to(Color)
+      with node:
+        box 10'pp, 10'pp, 80'pp, 80'pp
+        fill rgb(224, 239, 255).to(Color)
 
       rectangle "css grid area":
         # if current.gridTemplate != nil:
         #   echo "grid template: ", repr current.gridTemplate
         # setup frame for css grid
-        box 5'pp, 5'pp, 90'pp, 90'pp
-        # size 100'pp, 100'pp
-        fill "#FFFFFF"
-        cornerRadius 10
-        clipContent true
-        
-        # Setup CSS Grid Template
-        setGridCols 1'fr  1'fr  1'fr  1'fr  1'fr
-        setGridRows 1'fr 1'fr
-        justifyItems CxStart
+        with node:
+          box 5'pp, 5'pp, 90'pp, 90'pp
+          # size 100'pp, 100'pp
+          fill css"#FFFFFF"
+          cornerRadius 10
+          clipContent true
+          
+          # Setup CSS Grid Template
+          setGridCols 1'fr  1'fr  1'fr  1'fr  1'fr
+          setGridRows 1'fr 1'fr
+          justifyItems CxStart
 
         rectangle "item a":
           # Setup CSS Grid Template
-          cornerRadius 10
-          gridColumn 1 // 2
-          gridRow 1 // 3
-          # some color stuff
-          fill rgba(245, 129, 49, 123).to(Color)
+          with node:
+            cornerRadius 10
+            gridColumn 1 // 2
+            gridRow 1 // 3
+            # some color stuff
+            fill rgba(245, 129, 49, 123).to(Color)
 
         for i in 1..4:
           rectangle "items b", captures(i):
             # Setup CSS Grid Template
-            cornerRadius 6
-            # some color stuff
-            fill rgba(66, 177, 44, 167).to(Color).spin(i.toFloat*50)
+            with node:
+              cornerRadius 6
+              # some color stuff
+              fill rgba(66, 177, 44, 167).to(Color).spin(i.toFloat*50)
 
         rectangle "item e":
           # Setup CSS Grid Template
-          cornerRadius 6
-          gridColumn 5 // 6
-          gridRow 1 // 3
-          # some color stuff
-          fill rgba(245, 129, 49, 123).to(Color)
+          with node:
+            cornerRadius 6
+            gridColumn 5 // 6
+            gridRow 1 // 3
+            # some color stuff
+            fill rgba(245, 129, 49, 123).to(Color)
 
         # draw debug lines
         # gridTemplateDebugLines true
