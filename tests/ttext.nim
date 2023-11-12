9b8620b0d755b9c8f42f9b3244ce44d6d056c52b
Jaremy Creechley <creechley@gmail.com>
2023-11-12 00:33:51 -0700

    change apis

--- tests/ttext.nim
+++ tests/ttext.nim
@@ -23,17 +23,20 @@ proc draw*(self: Main) {.slot.} =
   nodes(self):
     rectangle "main":
       self.mainRect = current
-      box 10'ux, 10'ux, 600'ux, 120'ux
-      cornerRadius 10.0
-      fill "#2A9EEA".parseHtmlColor * 0.7
+      with node:
+        box 10'ux, 10'ux, 600'ux, 120'ux
+        cornerRadius 10.0
+        fill "#2A9EEA".parseHtmlColor * 0.7
       text "text":
-        box 10'ux, 10'ux, 400'ux, 100'ux
-        fill blackColor
-        setText({font: "hello world!\n",
-                 smallFont: "it's a small world"})
+        with node:
+          box 10'ux, 10'ux, 400'ux, 100'ux
+          fill blackColor
+          setText({font: "hello world!\n",
+                  smallFont: "it's a small world"})
       rectangle "main":
-        box 10'ux, 10'ux, 400'ux, 100'ux
-        fill whiteColor * 0.33
+        with node:
+          box 10'ux, 10'ux, 400'ux, 100'ux
+          fill whiteColor * 0.33
 
 var
   fig = Main.new()
