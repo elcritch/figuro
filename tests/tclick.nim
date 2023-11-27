7f1074b477e9a6e11542278c85f9d7e7510685b6
Jaremy Creechley <creechley@gmail.com>
2023-11-26 19:03:52 -0700

    updates

--- tests/tclick.nim
+++ tests/tclick.nim
@@ -82,6 +82,5 @@ proc tick*(self: Main, tick: int, time: MonoTime) {.slot.} =
   emit self.update()
 
 var main = Main.new()
-
 let frame = newAppFrame(main, size=(700'ui, 200'ui))
 startFiguro(frame)
