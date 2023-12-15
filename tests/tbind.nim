434bf8d85f50b2d7b8c9b748d0c5049291e6ff9a
Jaremy Creechley <creechley@gmail.com>
2023-12-15 16:51:09 -0700

    get rid of nodes

--- tests/tbind.nim
+++ tests/tbind.nim
@@ -14,45 +14,45 @@ type
     counter = Property[int]()
 
 proc draw*(self: Main) {.slot.} =
-  nodes(self):
-    self.name.setLen(0)
-    self.name.add "main"
+  self.name.setLen(0)
+  self.name.add "main"
+  with self:
+    fill css"#9F2B00"
+    size 100'pp, 100'pp
+
+  var node = self
+  rectangle "count":
     with node:
-      fill css"#9F2B00"
-      size 100'pp, 100'pp
-
-    rectangle "count":
+      cornerRadius 10.0
+      box 40'ux, 30'ux, 80'ux, 40'ux
+      fill css"#3B70DF"
+    text "btnText":
+      bindProp(self.counter)
       with node:
-        cornerRadius 10.0
-        box 40'ux, 30'ux, 80'ux, 40'ux
-        fill css"#3B70DF"
-      text "btnText":
-        bindProp(self.counter)
-        with node:
-          box 0'pp, 0'pp, 100'pp, 100'pp
-          fill blackColor
-          setText({font: $self.counter.value & " ₿" }, Center, Middle)
+        box 0'pp, 0'pp, 100'pp, 100'pp
+        fill blackColor
+        setText({font: $self.counter.value & " ₿" }, Center, Middle)
 
-    button "btnAdd":
-      box node, 160'ux, 30'ux, 80'ux, 40'ux
-      text "btnText":
-        with node:
-          size 100'pp, 100'pp
-          fill blackColor
-          setText({largeFont: "–"}, Center, Middle)
-      ## something like this:
-      self.counter.onSignal(doButton) do(counter: Property[int]):
-        counter.update(counter.value-1)
+  button "btnAdd":
+    box node, 160'ux, 30'ux, 80'ux, 40'ux
+    text "btnText":
+      with node:
+        size 100'pp, 100'pp
+        fill blackColor
+        setText({largeFont: "–"}, Center, Middle)
+    ## something like this:
+    self.counter.onSignal(doButton) do(counter: Property[int]):
+      counter.update(counter.value-1)
 
-    button "btnSub":
-      box node, 240'ux, 30'ux, 80'ux, 40'ux
-      text "btnText":
-        with node:
-          size 100'pp, 100'pp
-          fill blackColor
-          setText({largeFont: "+"}, Center, Middle)
-      self.counter.onSignal(doButton) do(counter: Property[int]):
-        counter.update(counter.value+1)
+  button "btnSub":
+    box node, 240'ux, 30'ux, 80'ux, 40'ux
+    text "btnText":
+      with node:
+        size 100'pp, 100'pp
+        fill blackColor
+        setText({largeFont: "+"}, Center, Middle)
+    self.counter.onSignal(doButton) do(counter: Property[int]):
+      counter.update(counter.value+1)
 
 
 var main = Main.new()
--- tests/tscroll.nim
+++ tests/tscroll.nim
@@ -16,30 +16,30 @@ proc hover*(self: Main, kind: EventKind) {.slot.} =
   refresh(self)
 
 proc draw*(self: Main) {.slot.} =
-  nodes self:
+  var node = self
+  with node:
+    fill css"#0000AA"
+  ScrollPane.new "scroll":
     with node:
-      fill css"#0000AA"
-    ScrollPane.new "scroll":
-      with node:
-        offset 2'pp, 2'pp
-        cornerRadius 7.0'ux
-        size 96'pp, 90'pp
-      node.settings.size.y = 20'ui
-      contents "children":
-        Vertical.new "":
-          # Setup CSS Grid Template
-          with node:
-            offset 10'ux, 10'ux
-            itemHeight cx"max-content"
-          for i in 0 .. 15:
-            Button.new "button", captures(i):
-              # current.gridItem = nil
-              with node:
-                size 1'fr, 50'ux
-                fill rgba(66, 177, 44, 197).to(Color).spin(i.toFloat*50)
-              if i in [3, 7]:
-                node.size 0.9'fr, 120'ux
-              node.connect(doHover, self, Main.hover)
+      offset 2'pp, 2'pp
+      cornerRadius 7.0'ux
+      size 96'pp, 90'pp
+    node.settings.size.y = 20'ui
+    contents "children":
+      Vertical.new "":
+        # Setup CSS Grid Template
+        with node:
+          offset 10'ux, 10'ux
+          itemHeight cx"max-content"
+        for i in 0 .. 15:
+          Button.new "button", captures(i):
+            # current.gridItem = nil
+            with node:
+              size 1'fr, 50'ux
+              fill rgba(66, 177, 44, 197).to(Color).spin(i.toFloat*50)
+            if i in [3, 7]:
+              node.size 0.9'fr, 120'ux
+            node.connect(doHover, self, Main.hover)
 
 var main = Main.new()
 let frame = newAppFrame(main, size=(600'ui, 480'ui))
