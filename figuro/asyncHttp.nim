0556a2b7970c7389a0775213e146a5b3ec4d1e96
Jaremy Creechley <creechley@gmail.com>
2024-04-11 15:45:16 +0300

    rename

---
+++ figuro/asyncHttp.nim
@@ -0,0 +1,68 @@
+import threading/channels
+import threading/smartptrs
+
+import std/os
+import std/options
+import std/isolation
+import std/uri
+import std/asyncdispatch
+
+import patty
+
+import meta
+import asyncProc
+
+export smartptrs
+export uri
+export asyncProc
+
+
+type
+  HttpRequest* = object
+    uri*: Uri
+  HttpResult* = object
+    data*: Option[string]
+
+  HttpProxy* = AgentProxy[HttpRequest, HttpResult]
+
+  HttpExecutor* = ref object of AsyncExecutor
+    proxy*: AgentProxy[HttpRequest, HttpResult]
+
+  ThreadAgent* = ref object of Agent
+
+  HttpAgent* = ref object of ThreadAgent
+    url: Uri
+
+proc send*(proxy: HttpProxy, agent: Agent, uri: string) =
+  let req = HttpRequest(uri: parseUri(uri))
+  proxy.sendMsg(agent, isolate req)
+
+proc newHttpExecutor*(proxy: HttpProxy): HttpExecutor =
+  result = HttpExecutor()
+  result.proxy = proxy
+
+method setup*(ap: HttpExecutor) {.gcsafe.} =
+  echo "setting up async http executor", " tid: ", getThreadId(), " trigger: ", ap.proxy[].trigger.repr 
+
+  let cb = proc (fd: AsyncFD): bool {.closure.} =
+    echo "\nrunning http executor event!"
+    var msg: AsyncMessage[HttpRequest]
+    if ap.proxy[].inputs.tryRecv(msg):
+      echo "got message: ", msg
+      let resp = HttpResult(data: some($msg.value.uri))
+      let res = AsyncMessage[HttpResult](handle: msg.handle, value: resp)
+      ap.proxy[].outputs.send(res)
+
+  ap.proxy[].trigger.addEvent(cb)
+
+proc receive*(proxy: HttpProxy, ap: Agent, data: HttpResult) {.gcsafe.} =
+  echo "http executor receive: "
+
+proc newHttpAgent*(url: Uri): HttpAgent =
+  result = HttpAgent(url: url)
+
+proc newHttpAgent*(url: string): HttpAgent =
+  newHttpAgent(parseUri(url))
+
+proc update*(req: HttpAgent, gotByts: int) {.signal.}
+proc received*(req: HttpAgent, val: string) {.signal.}
--- figuro/asyncs.nim
+++ figuro/asyncs.nim
@@ -2,6 +2,7 @@ import threading/channels
 import threading/smartptrs
 
 import std/os
+import std/monotimes
 import std/options
 import std/isolation
 import std/uri
@@ -10,59 +11,112 @@ import std/asyncdispatch
 import patty
 
 import meta
-import asyncProc
 
 export smartptrs
 export uri
-export asyncProc
-
 
 type
-  HttpRequest* = object
-    uri*: Uri
-  HttpResult* = object
-    data*: Option[string]
+  AsyncReqId* = int
 
-  HttpProxy* = AgentProxy[HttpRequest, HttpResult]
+  AsyncKey* = object
+    reqId*: AsyncReqId
+    aid*: AgentId
 
-  HttpExecutor* = ref object of AsyncExecutor
-    proxy*: AgentProxy[HttpRequest, HttpResult]
+  AsyncMessage*[T] = object
+    continued*: bool
+    handle*: AsyncKey
+    value*: T
 
-  ThreadAgent* = ref object of Agent
+  AgentProxyRaw*[T, U] = object
+    agents*: Table[AsyncKey, Agent]
+    inputs*: Chan[AsyncMessage[T]]
+    outputs*: Chan[AsyncMessage[U]]
+    trigger*: AsyncEvent
 
-  HttpAgent* = ref object of ThreadAgent
-    url: Uri
+  AgentProxy*[T, U] = SharedPtr[AgentProxyRaw[T, U]]
 
-proc send*(proxy: HttpProxy, agent: Agent, uri: string) =
-  let req = HttpRequest(uri: parseUri(uri))
-  proxy.sendMsg(agent, isolate req)
+  AsyncExecutor* {.acyclic.} = ref object of RootObj
 
-proc newHttpExecutor*(proxy: HttpProxy): HttpExecutor =
-  result = HttpExecutor()
-  result.proxy = proxy
+method setup*(ap: AsyncExecutor) {.base, gcsafe.} =
+  discard
 
-method setup*(ap: HttpExecutor) {.gcsafe.} =
-  echo "setting up async http executor", " tid: ", getThreadId(), " trigger: ", ap.proxy[].trigger.repr 
+# method processOutputs*(ap: AsyncExecutor) {.base, gcsafe.} =
+#   discard
 
+variant Commands:
+  Finish
+  AddExec(exec: AsyncExecutor)
+
+type
+  AsyncProcessorRaw* = object
+    commands*: Chan[Commands]
+    thread*: Thread[SharedPtr[AsyncProcessorRaw]]
+    trigger*: AsyncEvent
+
+  AsyncProcessor* = SharedPtr[AsyncProcessorRaw]
+
+  AsyncMethod*[T, U] = ref object of RootObj
+
+proc initAsyncKey*(agent: Agent): AsyncKey =
+  AsyncKey(aid: agent.getId(), reqId: getMonoTime().ticks().int)
+
+proc newAsyncProcessor*(): AsyncProcessor =
+  result = newSharedPtr(AsyncProcessorRaw)
+  result[].commands = newChan[Commands]()
+  result[].trigger = newAsyncEvent()
+
+proc execute*(ap: AsyncProcessor) {.thread.} =
   let cb = proc (fd: AsyncFD): bool {.closure.} =
-    echo "\nrunning http executor event!"
-    var msg: AsyncMessage[HttpRequest]
-    if ap.proxy[].inputs.tryRecv(msg):
-      echo "got message: ", msg
-      let resp = HttpResult(data: some($msg.value.uri))
-      let res = AsyncMessage[HttpResult](handle: msg.handle, value: resp)
-      ap.proxy[].outputs.send(res)
+    echo "running async processor command!"
+    var cmd: Commands
+    if ap[].commands.tryRecv(cmd):
+      match cmd:
+        Finish:
+          echo "stopping exec"
+          raise newException(CatchableError, "finish")
+        AddExec(exec):
+          echo "adding exec: ", repr exec
+          setup(exec)
+  ap[].trigger.addEvent(cb)
 
-  ap.proxy[].trigger.addEvent(cb)
+  try:
+    runForever()
+  except CatchableError:
+    echo "done"
 
-proc receive*(proxy: HttpProxy, ap: Agent, data: HttpResult) {.gcsafe.} =
-  echo "http executor receive: "
+proc start*(ap: AsyncProcessor) =
+  createThread(ap[].thread, execute, ap)
 
-proc newHttpAgent*(url: Uri): HttpAgent =
-  result = HttpAgent(url: url)
+proc finish*(ap: AsyncProcessor) =
+  ap[].commands.send(Finish())
+  ap[].trigger.trigger()
 
-proc newHttpAgent*(url: string): HttpAgent =
-  newHttpAgent(parseUri(url))
+proc add*(ap: AsyncProcessor, exec: sink AsyncExecutor) =
+  ap[].commands.send(unsafeIsolate AddExec(exec))
+  ap[].trigger.trigger()
 
-proc update*(req: HttpAgent, gotByts: int) {.signal.}
-proc received*(req: HttpAgent, val: string) {.signal.}
+proc newAgentProxy*[T, U](): AgentProxy[T, U] =
+  result = newSharedPtr(AgentProxyRaw[T, U])
+  result[].inputs = newChan[AsyncMessage[T]]()
+  result[].outputs = newChan[AsyncMessage[U]]()
+  result[].trigger = newAsyncEvent()
+
+proc sendMsg*[T, U](proxy: AgentProxy[T, U], agent: Agent, val: sink Isolated[T]) =
+  let rkey = initAsyncKey(agent)
+  let msg = AsyncMessage[T](handle: rkey, value: val.extract())
+  proxy[].agents[rkey] = agent
+  proxy[].inputs.send(msg)
+  proxy[].trigger.trigger()
+
+template sendMsg*[T, U](proxy: AgentProxy[T, U], agent: Agent, val: T) =
+  sendMsg(proxy, agent, isolate(val))
+
+proc process*[T, U](proxy: AgentProxy[T, U], maxCnt = 20) =
+  mixin receive
+  var cnt = maxCnt
+  var msg: AsyncMessage[U]
+  while proxy[].outputs.tryRecv(msg) and cnt > 0:
+    let agent: Agent = proxy[].agents[msg.handle]
+    if not msg.continued:
+      proxy[].agents.del(msg.handle)
+    proxy.receive(agent, msg.value)
