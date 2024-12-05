b3df749c72a6c5b9166b69da8df0168a3c7336ad
Jaremy Creechley <creechley@gmail.com>
2024-12-05 03:01:31 -0700

    css parsing

--- tests/tstylus.nim
+++ tests/tstylus.nim
@@ -2,23 +2,62 @@
 import std/os, stylus
 
 const src = """
-/* Hello CSS! 
-   This is some basic CSS code to test Stylus' tokenizer!
-   Currently, it can support comments, functions, numericals, and more!
-*/
-body {
-  /* Sets the color to a linear gradient */
-  color: linear-gradient(45deg, red, blue);
-  
-  /* Sets `awesomeness` to 45% */
-  awesomemess: 45%;
-
-  /* Sets the background image to a URL that our browser will fetch */
-  background-image: url(https://ferus.org/assets/h2kaofwlweqeJWmq.png);
+Button {
+  color: rgba(10, 10, 10, 100);
 }
 """
 
 let tokenizer = newTokenizer(src)
 
-while not tokenizer.isEof():
-  echo repr tokenizer.nextToken()
+type
+  CssParser* = ref object
+    buff: seq[Token]
+    tokenizer: Tokenizer
+
+proc peek(parser: CssParser): Token =
+  if tokenizer.isEof():
+    raise newException(Exception, "EOF!")
+  if parser.buff.len() == 0:
+    parser.buff.add(parser.tokenizer.nextToken())
+  parser.buff[0]
+
+proc nextToken(parser: CssParser): Token =
+  if tokenizer.isEof():
+    raise newException(Exception, "EOF!")
+  if parser.buff.len() == 0:
+    parser.tokenizer.nextToken()
+  else:
+    let tk = parser.buff[0]
+    parser.buff.del(0)
+    tk
+
+proc eat*(parser: CssParser, kind: TokenKind): bool =
+  if tokenizer.isEof():
+    raise newException(Exception, "EOF!")
+  let tk = parser.nextToken()
+  if tk.kind != kind:
+    raise newException(Exception, "Expected: " & $kind & " got: " & $tk.kind)
+
+proc skip*(parser: CssParser, kind: TokenKind = tkWhiteSpace) =
+  while not tokenizer.isEof():
+    let tk = parser.peek()
+    if tk.kind == kind:
+      echo "skip whitespace"
+      discard parser.nextToken()
+      continue
+    else:
+      echo "not whitespace"
+      break
+
+proc parse*(parser: CssParser) =
+  parser.skip(tkWhiteSpace)
+
+  let tk = parser.nextToken()
+  echo repr tk
+
+  echo "rest:"
+  while not tokenizer.isEof():
+    echo repr tokenizer.nextToken()
+
+let parser = CssParser(tokenizer: tokenizer)
+parse(parser)
