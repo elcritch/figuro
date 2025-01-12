7b61c8d451dc249776270a6927ebbe9a3a93dd74
Jaremy Creechley <creechley@gmail.com>
2025-01-12 03:09:59 -0700

    fix connects

--- figuro/runtime/cssMonitor.nim
+++ figuro/runtime/cssMonitor.nim
@@ -23,16 +23,13 @@ else:
 
 var watcherSelf: WeakRef[CssLoader]
 
-proc themeUpdate() =
+proc fsmonCallback(event: fsw_cevent, eventNum: cuint) =
   let cssRules = loadTheme()
   if cssRules.len() > 0:
     echo "CSSTheme updated: ", themePath()
     emit watcherSelf.cssUpdate(cssRules)
     os.sleep(16) # TODO: fixme: this is a hack to ensure proper text resizing 
     emit watcherSelf.cssUpdate(cssRules)
-
-proc fsmonCallback(event: fsw_cevent, eventNum: cuint) =
-  themeUpdate()
 
 proc cssWatcherImpl*(self: CssLoader) =
   watcherSelf = self.unsafeWeakRef()
@@ -56,7 +53,16 @@ proc cssLoaderImpl*(self: CssLoader) =
 
 proc cssLoader*(self: CssLoader) {.slot.} =
   echo "Starting CSS Loader"
-  cssLoaderImpl(self)
+  echo "css start"
+  while true:
+    echo "CSSTheme check"
+    let cssRules = loadTheme()
+    if cssRules.len() > 0:
+      echo "CSSTheme updated: ", themePath()
+      emit self.cssUpdate(cssRules)
+      os.sleep(16) # TODO: fixme: this is a hack to ensure proper text resizing 
+      emit watcherSelf.cssUpdate(cssRules)
+    os.sleep(300_000)
 
 proc cssWatcher*(self: CssLoader) {.slot.} =
   echo "Starting CSS Watcher"
