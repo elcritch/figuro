4ac5e0b1a28d5a3f62ddf708f0658ef8a4fff470
Jaremy Creechley <creechley@gmail.com>
2025-01-13 02:25:52 -0700

    tweak tanimate

--- tests/tanimate.nim
+++ tests/tanimate.nim
@@ -13,29 +13,30 @@ type
 
 proc tick*(self: Main, now: MonoTime, delta: Duration) {.slot.} =
   refresh(self)
-  self.value += 0.0013 * delta.inMilliseconds.toFloat
+  self.value += 0.00013 * delta.inMilliseconds.toFloat
   self.value = clamp(self.value mod 1.0, 0, 1.0)
 
 proc draw*(self: Main) {.slot.} =
   let node = self
   rectangle "main":
-    box node, 0'ui, 0'ui, 620'ui, 140'ui
+    box node, -100'ui, 0'ui, 600'ui, 140'ui
     let j = 1
-    for i in 0 .. 5:
+    for i in 0 .. 9:
       capture i, j:
         Button.new "btn":
           let value = self.value
-          fill node, css"#AA0000"
+          fill node, css"#AA0000".spin(i.toFloat*20.0)
           node.onHover:
-            fill node, css"#F00000"
+            fill node, node.fill.lighten(0.1)
+          let xval = (i.toFloat * 60 + value*600) mod 600.0
           box node,
-              ux(20 + (i.toFloat + value) * 120),
-              ux(30 + 20 * sin(value + i.toFloat)),
+              ux(xval),
+              ux(30 + 20 * sin(xval/640*2*3.14)),
               60'ui, 60'ui
           if i == 0:
             node.fill.a = value * 1.0
 
 var fig = Main.new()
 
-var frame = newAppFrame(fig, size=(720'ui, 140'ui))
+var frame = newAppFrame(fig, size=(500'ui, 140'ui))
 startFiguro(frame)
