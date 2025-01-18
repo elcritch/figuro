5709683686b0fcafd21b69872c33d03dd03a62ce
Jaremy Creechley <creechley@gmail.com>
2025-01-18 04:42:09 -0700

    updates

--- examples/hnloader.nim
+++ examples/hnloader.nim
@@ -1,6 +1,7 @@
 import sigils
 import std/httpclient
 import std/os
+import std/sequtils
 import std/strutils
 
 import chame/minidom
@@ -9,6 +10,9 @@ import pretty
 type HtmlLoader* = ref object of Agent
   url: string
 
+proc parseTable(mainTable: Element) =
+  echo "main table: ", elem.localNameStr()
+
 
 proc loadPage(loader: HtmlLoader) {.slot.} =
   echo "Starting page load..."
@@ -16,14 +20,21 @@ proc loadPage(loader: HtmlLoader) {.slot.} =
   let res = client.get(loader.url)
   let document = parseHTML(res.bodyStream)
   var stack = @[Node(document)]
+
   while stack.len > 0:
     let node = stack.pop()
     if node of minidom.Text:
       let s = minidom.Text(node)
-      echo "text: ", s.data.strip()
+      # echo "text: ", s.data.strip()
     elif node of minidom.Element:
       let elem = minidom.Element(node)
-      echo "element: ", elem.localNameStr()
+      if elem.localNameStr == "table":
+        echo "element: ", elem.localNameStr()
+        # echo "element: ", elem.attrsStr().toSeq()
+        for name, value in elem.attrsStr():
+          if name == "id":
+            echo "element: found table "
+            break
     for i in countdown(node.childList.high, 0):
       stack.add(node.childList[i])
 
