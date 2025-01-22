571b488c67849383aa3a22bc3cc13a4b8a94c76a
Jaremy Creechley <creechley@gmail.com>
2025-01-21 22:22:14 -0700

    fixing things

--- figuro/widgets/basics.nim
+++ figuro/widgets/basics.nim
@@ -2,15 +2,9 @@ import commons
 import ../widget
 export widget
 
-# type Rectangle* = ref object of BasicFiguro
-
 proc draw*(self: Rectangle) {.slot.} =
   ## button widget!
   discard
-
-# exportWidget(basicRectangle, Rectangle)
-
-# type Text* = ref object of BasicFiguro
 
 proc draw*(self: Text) {.slot.} =
   ## text widget!
