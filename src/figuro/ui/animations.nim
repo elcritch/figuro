3c820e87acf23c18387573d9f26d44f6f315070b
Jaremy Creechley <creechley@gmail.com>
2025-01-24 01:28:56 -0700

    fade change mid fade

--- src/figuro/ui/animations.nim
+++ src/figuro/ui/animations.nim
@@ -4,33 +4,6 @@ import pkg/chronicles
 import ../commons
 import apis
 
-type FadeAnimation* = object
-  minMax*: Slice[float]
-  incr*: float
-  decr*: float
-  active*: bool = false
-  amount*: float = 0.0
-
-proc tick*(self: var FadeAnimation, node: Figuro): bool {.discardable.} =
-  if self.active and self.amount < self.minMax.b:
-    self.amount += self.incr
-    refresh(node)
-    result = true
-  elif not self.active and self.amount > self.minMax.a:
-    self.amount -= self.decr
-    refresh(node)
-    result = true
-
-proc isActive*(self: var FadeAnimation, isActive = true) =
-  self.active = isActive
-
-proc setMax*(self: var FadeAnimation) =
-  self.amount = self.minMax.b
-
-proc setMin*(self: var FadeAnimation) =
-  self.amount = self.minMax.a
-
-
 type Fader* = ref object of Agent
   active*: bool = false
   fadingIn*: bool = false
