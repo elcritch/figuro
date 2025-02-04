3c36fa8cf8c3ad0d86c57b0b2e92f26685e54b9e
Jaremy Creechley <creechley@gmail.com>
2025-02-04 02:19:11 -0700

    bump version

--- tests/tbutton.nim
+++ tests/tbutton.nim
@@ -12,29 +12,28 @@ type
     mainRect: Figuro
 
 proc draw*(self: Main) {.slot.} =
-  with self:
-    setName "main"
-    fill css"#9F2B00"
-    box 0'ux, 0'ux, 400'ux, 300'ux
+  withWidget(self):
+    with self:
+      setName "main"
+      fill css"#9F2B00"
+      box 0'ux, 0'ux, 400'ux, 300'ux
 
-  let node = self
-  Button[int] as "btn":
-    with this:
-      box 40'ux, 30'ux, 80'ux, 80'ux
-      fill css"#2B9F2B"
-    
-    node.shadow[DropShadow] = Shadow(blur: 4.0'ui, x: 2.0'ui, y: 2.0'ui,
-                                color: Color(r: 0.0, g: 0.0, b: 0.0, a: 0.1))
-    node.shadow[InnerShadow] = Shadow(blur: 8.0'ui, x: 3.0'ui, y: 3.0'ui,
-                                color: Color(r: 1.0, g: 1.0, b: 1.0, a: 0.5))
-
-    Text.new "btnText":
+    Button[int] as "btn":
       with this:
-        box 10'ux, 10'ux, 80'pp, 80'pp
-        fill blackColor
-        justify Center
-        align Middle
-        text({font: "testing"})
+        box 40'ux, 30'ux, 80'ux, 80'ux
+        fill css"#2B9F2B"
+      
+      this.shadow[DropShadow] = Shadow(blur: 4.0'ui, x: 2.0'ui, y: 2.0'ui,
+                                  color: Color(r: 0.0, g: 0.0, b: 0.0, a: 0.1))
+      this.shadow[InnerShadow] = Shadow(blur: 8.0'ui, x: 3.0'ui, y: 3.0'ui,
+                                  color: Color(r: 1.0, g: 1.0, b: 1.0, a: 0.5))
+
+      Text.new "btnText":
+        with this:
+          foreground blackColor
+          justify Center
+          align Middle
+          text({font: "testing"})
 
 
 var main = Main.new()
