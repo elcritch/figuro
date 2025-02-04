e73944278b1e90cc559329e79ec796fc6632c411
Jaremy Creechley <creechley@gmail.com>
2025-02-04 01:41:01 -0700

    rename ndoe to this

--- src/figuro/renderer/opengl/renderer.nim
+++ src/figuro/renderer/opengl/renderer.nim
@@ -64,7 +64,7 @@ proc renderText(ctx: Context, node: Node) {.forbids: [AppMainThreadEff].} =
 
     let
       glyphId = glyph.hash()
-      charPos = vec2(glyph.pos.x, glyph.pos.y - glyph.descent*7/8)
+      charPos = vec2(glyph.pos.x, glyph.pos.y - glyph.descent*5/6)
     if glyphId notin ctx.entries:
       echo "no glyph in context: ",
         glyphId, " glyph: `", glyph.rune, "`", " (", repr(glyph.rune), ")"
