f3e34eb36e177ed5c1108a1484f6fe7593a1bc26
Jaremy Creechley <creechley@gmail.com>
2025-02-04 02:26:40 -0700

    rename ndoe to this

--- tests/tslider.nim
+++ tests/tslider.nim
@@ -21,45 +21,47 @@ proc deactivateSlider*(self: Main) {.slot.} =
   self.bkgFade.fadeOut()
 
 proc draw*(self: Main) {.slot.} =
-  let node = self
-  rectangle "body":
-    with this:
-      fill rgba(66, 177, 44, 197).to(Color).spin(100).darken(0.3*self.bkgFade.amount)
-      zlevel 20.ZLevel
-      box ux(140*self.bkgFade.amount - 140), 0'ux, 140'ux, 100'pp
-      cornerRadius 0.0
-    Vertical.new "menu":
-      box node, 0'ux, 10'ux, 100'pp, 95'pp
-      contentHeight node, cx"min-content", gap = 20'ui
-      Button.new "Close":
-        size node, 120'ux, 40'ux
-        connect(node, doClicked, self, deactivateSlider)
+  withWidget(self):
+    rectangle "body":
+      with this:
+        fill rgba(66, 177, 44, 197).to(Color).spin(100).darken(0.3*self.bkgFade.amount)
+        zlevel 20.ZLevel
+        box ux(140*self.bkgFade.amount - 140), 0'ux, 140'ux, 100'pp
+        cornerRadius 0.0
+      Vertical.new "menu":
+        box this, 0'ux, 10'ux, 100'pp, 95'pp
+        contentHeight this, cx"min-content", gap = 20'ui
+        Button.new "Close":
+          size this, 120'ux, 40'ux
+          connect(this, doClicked, self, deactivateSlider)
+          Text.new "text":
+            with this:
+              foreground blackColor
+              justify Center
+              align Middle
+              text({defaultFont: "Close Menu"})
+    Horizontal.new "horiz":
+      offset this, 30'pp, 0'ux
+      contentWidth this, cx"min-content", gap = 20'ui
+      Button.new "Open":
+        size this, 120'ux, 60'ux
+        connect(this, doClicked, self, activateSlider)
         Text.new "text":
-          offset node, 00'ux, 7'ux
-          fill node, blackColor
-          justify Center
-          align Middle
-          text({defaultFont: "Close Menu"})
-  Horizontal.new "horiz":
-    offset node, 30'pp, 0'ux
-    contentWidth node, cx"min-content", gap = 20'ui
-    Button.new "Open":
-      size node, 120'ux, 60'ux
-      connect(node, doClicked, self, activateSlider)
-      Text.new "text":
-        fill node, blackColor
-        justify Center
-        align Middle
-        text({defaultFont: "Open Menu"})
+          with this:
+            foreground blackColor
+            justify Center
+            align Middle
+            text({defaultFont: "Open Menu"})
 
-    Button.new "Close":
-      size node, 120'ux, 60'ux
-      connect(node, doClicked, self, deactivateSlider)
-      Text.new "text":
-        fill node, blackColor
-        justify Center
-        align Middle
-        text({defaultFont: "Close Menu"})
+      Button.new "Close":
+        size this, 120'ux, 60'ux
+        connect(this, doClicked, self, deactivateSlider)
+        Text.new "text":
+          with this:
+            foreground blackColor
+            justify Center
+            align Middle
+            text({defaultFont: "Close Menu"})
 
 var main = Main.new()
 var frame = newAppFrame(main, size=(800'ui, 600'ui))
