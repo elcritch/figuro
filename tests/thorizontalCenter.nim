3d19268bb0c504ecdd8fb2cf33e85b707d517c06
Jaremy Creechley <creechley@gmail.com>
2025-02-23 21:08:42 -0700

    updates

--- tests/thorizontalCenter.nim
+++ tests/thorizontalCenter.nim
@@ -2,6 +2,7 @@
 ## This minimal example shows 5 blue squares.
 import figuro/widgets/[button, horizontal, griddebug]
 import figuro
+import cssgrid/prettyprints
 
 type
   Main* = ref object of Figuro
@@ -25,33 +26,34 @@ proc draw*(self: Main) {.slot.} =
   withRootWidget(self):
     Rectangle.new "body":
       with this:
-        box 5'pp, 5'pp, 90'pp, 600'ux
+        box 5'pp, 5'pp, 400'ux, 600'ux
         cornerRadius 10.0'ui
         fill whiteColor.darken(self.hoveredAlpha)
         border 3'ui, blueColor
         
-      GridDebug.new "debug-grid":
-        this.state = (blackColor, "horiz")
-      GridDebug.new "debug-grid":
-        this.state = (blackColor, "horiz2")
+      # GridDebug.new "debug-grid":
+      #   this.state = (blackColor, "horiz")
+      # GridDebug.new "debug-grid":
+      #   this.state = (blackColor, "horiz2")
 
-      Horizontal.new "horiz":
-        offset this, 0'ux, 0'ux
-        size this, 100'pp, 200'ux
-        contentWidth this, 1'fr, gap = 20'ui
-        border this, 3'ui, css"#00ff00"
-        for i in 0 .. 3:
-          capture i:
-            Button[int].new "btn":
-              with this:
-                size 100'ux, 100'ux
-                # we need to connect the nodes onHover event
-              connect(doHover, self, buttonHover)
+      # Horizontal.new "horiz":
+      #   offset this, 0'ux, 0'ux
+      #   size this, 100'pp, 200'ux
+      #   contentWidth this, 1'fr, gap = 20'ui
+      #   border this, 3'ui, css"#00ff00"
+      #   for i in 0 .. 3:
+      #     capture i:
+      #       Button[int].new "btn":
+      #         with this:
+      #           size 100'ux, 100'ux
+      #           # we need to connect the nodes onHover event
+      #         connect(doHover, self, buttonHover)
 
       Horizontal.new "horiz2":
         offset this, 0'pp, 200'ux
-        size this, 100'pp, 20'pp
-        contentWidth this, cx"max-content", gap = 20'ui
+        # size this, 100'pp, 20'pp
+        size this, 400'ux, 120'ux
+        contentWidth this, cx"auto", gap = 0'ui
         border this, 3'ui, css"#ff0000"
         for i in 0 .. 3:
           capture i:
@@ -61,6 +63,8 @@ proc draw*(self: Main) {.slot.} =
                 size 50'ux, 50'ux
                 # we need to connect the nodes onHover event
               connect(doHover, self, buttonHover)
+    # prettyPrintWriteMode = cmTerminal
+    printLayout(this, cmTerminal)
 
 var main = Main.new()
 var frame = newAppFrame(main, size=(720'ui, 640'ui))
