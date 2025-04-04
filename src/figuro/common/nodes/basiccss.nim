fcb563a5c2ada8d524abfcfcf7304835bdd57d53
Jaremy Creechley <creechley@gmail.com>
2025-04-04 00:27:38 -0600

    add cssvars

--- src/figuro/common/nodes/basiccss.nim
+++ src/figuro/common/nodes/basiccss.nim
@@ -19,18 +19,18 @@ variantp CssValue:
   CssAttribute(a: string)
 
 type
-  CssVars* = ref object of CssVariables
-    parent*: CssVars
+  CssValues* = ref object of CssVariables
+    parent*: CssValues
     values*: Table[CssVarId, CssValue]
     valuesNames*: Table[string, CssVarId]
 
-proc resolveVariable*(vars: CssVars, varIdx: CssVarId, val: var ConstraintSize): bool =
+proc resolveVariable*(vars: CssValues, varIdx: CssVarId, val: var ConstraintSize): bool =
   if vars.resolveVariable(varIdx, val):
     result = true
   elif vars.parent != nil:
     result = vars.parent.resolveVariable(varIdx, val)
 
-proc resolveVariable*(vars: CssVars, varIdx: CssVarId, val: var CssValue): bool =
+proc resolveVariable*(vars: CssValues, varIdx: CssVarId, val: var CssValue): bool =
   ## Resolves a constraint size, looking up variables if needed
   if vars != nil and varIdx in vars.values:
     var res = vars.values[varIdx]
@@ -54,13 +54,6 @@ proc resolveVariable*(vars: CssVars, varIdx: CssVarId, val: var CssValue): bool 
   else:
     return false
 
-
-when false:
-  let cssVars = newCssVariables()
-  let baseVar = cssVars.registerVariable("base", 50'ux)
-  proc lookupVariable*(vars: CssVariables, idx: int, size: var ConstraintSize): bool
-  proc resolveVariable*(vars: CssVariables, cx: Constraint): Constraint
-  let resolvedSize = cssVars.resolveVariable(ConstraintSize(kind: UiVariable, varIdx: varIdx))
 
 
 type
