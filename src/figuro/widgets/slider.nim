8dcbf09d78c851da12db4d37aa8c0ca7fa0a6075
Jaremy Creechley <creechley@gmail.com>
2025-04-07 02:35:33 -0600

    adding slider - exper

--- src/figuro/widgets/slider.nim
+++ src/figuro/widgets/slider.nim
@@ -11,7 +11,7 @@ type
     dragStart*: T
     buttonSize*, fillingSize*, halfSize*: CssVarId
 
-proc sliderDrag*[T](
+proc buttonDrag*[T](
     self: Slider[T],
     kind: EventKind,
     initial: Position,
@@ -19,10 +19,17 @@ proc sliderDrag*[T](
     overlaps: bool,
     selected: Figuro
 ) {.slot.} =
-  trace "scrollBarDrag: ", name = self.name, kind = kind, initial = initial, cursor = cursor, overlaps = overlaps, selected = selected != self
+  debug "slider:buttonDrag: ", name = self.name, kind = kind, initial = initial, cursor = cursor, overlaps = overlaps, selected = selected != self
   case kind:
   of Exit:
     self.dragStart = self.min
+    if initial == cursor:
+      let bar = self.queryChild("bar").get()
+      let rel = initial.positionRelative(bar)
+      let offset = float(rel[dcol] / bar.box.w)
+      self.state = clamp(self.dragStart + offset, self.min, self.max)
+      emit self.doUpdate(self.state)
+      refresh(self)
   of Init:
     self.dragStart = self.state
     discard
@@ -57,13 +64,14 @@ proc draw*[T](self: Slider[T]) {.slot.} =
     printLayout(self, cmTerminal, self.frame[].theme.css.values)
     debug "slider:draw", name = self.name, buttonSize = self.buttonSize, fillingSize = self.fillingSize, cssValues = self.frame[].theme.css.values.values, cssVariables = self.frame[].theme.css.values.variables
 
-    gridCols csVar(self.buttonSize, self.halfSize) ["left"] 1'fr ["right"] csVar(self.buttonSize, self.halfSize)
+    gridCols 0'ux ["left"] 1'fr ["right"] 0'ux
     gridRows 1'fr ["top"] csVar(self.buttonSize) ["bottom"] 1'fr
 
     let sliderWidth = csPerc(100 * self.state.float.clamp(self.min.float, self.max.float))
 
     Rectangle.new "bar":
       gridArea 2 // 3, 2 // 3
+      uinodes.connect(this, doDrag, self, buttonDrag)
 
       Rectangle.new "filling":
         # Draw the bar itself.
@@ -71,12 +79,17 @@ proc draw*[T](self: Slider[T]) {.slot.} =
         size sliderWidth, csVar(self.fillingSize)
         offset 0'ux, csVar(self.buttonSize, self.halfSize) - csVar(self.fillingSize, self.halfSize)
 
-      Rectangle.new "button":
-        fill css"black" * 0.7
+      Rectangle.new "button-bg":
         size csVar(self.buttonSize), csVar(self.buttonSize)
         offset sliderWidth-csVar(self.buttonSize, self.halfSize), 0'ux
-        uinodes.connect(this, doDrag, self, sliderDrag)
-      
+        border 1'ui, css"red"
+
+        Rectangle.new "button":
+          fill css"black" * 0.7
+          size 100'pp, 100'pp
+          offset 10'ux, 0'ux
+          uinodes.connect(this, doDrag, self, buttonDrag)
+
     WidgetContents()
 
 
