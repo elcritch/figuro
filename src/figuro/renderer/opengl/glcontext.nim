1b6356c7f7054bfe0266a2532bdd8821ca370495
Jaremy Creechley <creechley@gmail.com>
2025-05-08 19:21:13 -0600

    add direction enum

--- src/figuro/renderer/opengl/glcontext.nim
+++ src/figuro/renderer/opengl/glcontext.nim
@@ -717,31 +717,42 @@ proc fillRoundedRect*(ctx: Context, rect: Rect, color: Color, radius: float32) =
 
   if radius > 0.0:
     # let stroked = stroked and lineWidth <= radius
-    var hashes: array[4, Hash]
-    for quadrant in 1 .. 4:
-      let qhash = hash !& quadrant
-      hashes[quadrant - 1] = qhash
+    var hashes: array[DirectionCorners, Hash]
+    for quadrant in DirectionCorners:
+      let qhash = hash !& quadrant.int
+      hashes[quadrant] = qhash
 
-    if hashes[0] notin ctx.entries:
+    if hashes[dcTopLeft] notin ctx.entries:
       let circle = generateCircle(radius.int, stroked = false)
       let patches = sliceToNinePatch(circle)
       # Store each piece in the atlas
       let patchArray = [
-        patches.topRight, 
-        patches.topLeft,
-        patches.bottomLeft,
-        patches.bottomRight,
+        dcTopLeft: patches.topLeft,
+        dcTopRight: patches.topRight, 
+        dcBottomRight: patches.bottomRight,
+        dcBottomLeft: patches.bottomLeft,
       ]
 
-      for quadrant in 1 .. 4:
-        let img = patchArray[quadrant - 1]
-        ctx.putImage(hashes[quadrant - 1], img)
+      for quadrant in DirectionCorners:
+        let img = patchArray[quadrant]
+        ctx.putImage(hashes[quadrant], img)
 
     let
       xy = rect.xy
-      offsets = [vec2(w - rw, 0), vec2(0, 0), vec2(0, h - rh), vec2(w - rw, h - rh)]
+      # offsets = [
+      #   TR: vec2(w - rw, 0),
+      #   TL: vec2(0, 0),
+      #   BL: vec2(0, h - rh),
+      #   BR: vec2(w - rw, h - rh)
+      #   ]
+      offsets = [
+        dcTopLeft: vec2(0, 0),
+        dcTopRight: vec2(w - rw, 0),
+        dcBottomRight: vec2(w - rw, h - rh),
+        dcBottomLeft: vec2(0, h - rh),
+      ]
 
-    for corner in 0 .. 3:
+    for corner in DirectionCorners:
       let
         uvRect = ctx.entries[hashes[corner]]
         wh = rect.wh * ctx.atlasSize.float32
