56a238c9f9973ae86e8098e13e71dccb1ae57eb8
Jaremy Creechley <creechley@gmail.com>
2025-05-09 21:30:52 -0600

    refactor circlebox

--- src/figuro/renderer/opengl/glcontext.nim
+++ src/figuro/renderer/opengl/glcontext.nim
@@ -1084,7 +1084,7 @@ proc fillRoundedRectWithShadow*(
             lineWidth = 1.0,
             innerShadow = true,
             outerShadow = false,
-            innerShadowBorder = false,
+            innerShadowBorder = true,
             outerShadowFill = false,
           )
           mainImg.writeFile("examples/renderer-shadowImage-" & $innerShadow & ".png")
