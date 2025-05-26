f1a2c5d3dfc46e550b4126be74e8ca48d966ba9c
Jaremy Creechley <creechley@gmail.com>
2025-05-25 22:38:55 -0600

    updates

--- src/figuro/renderer/boxy/drawextras.nim
+++ src/figuro/renderer/boxy/drawextras.nim
@@ -30,10 +30,6 @@ proc drawRoundedRect*(
   if rect.w <= 0 or rect.h <= -0:
     return
 
-  if radii == [0.0'f32, 0.0, 0.0, 0.0]:
-    bxy.drawRect(rect, color)
-    return
-
   let
     w = rect.w.ceil()
     h = rect.h.ceil()
