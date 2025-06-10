e9d36ee5ff085752d5187ab610d44cc3755c3ae1
Jaremy Creechley <creechley@gmail.com>
2025-06-10 01:47:34 -0600

    update sdfy

--- src/figuro/renderer/utils/drawshadows.nim
+++ src/figuro/renderer/utils/drawshadows.nim
@@ -219,7 +219,7 @@ proc fillRoundedRectWithShadowSdf*[R](
                   params = RoundedBoxParams(r: corners),
                   pos = whiteColor,
                   neg = whiteColor,
-                  factor = shadowBlur * 0.7,
+                  factor = shadowBlur * 0.75,
                   spread = shadowSpread,
                   mode = sdfModeDropShadow)
           shadowCache[mainKey] = shadowImg
