3c9c580781b28348ef28da32877bcd3c35995b65
Jaremy Creechley <creechley@gmail.com>
2025-06-10 02:11:14 -0600

    update sdfy

--- src/figuro/renderer/utils/drawshadows.nim
+++ src/figuro/renderer/utils/drawshadows.nim
@@ -223,7 +223,7 @@ proc fillRoundedRectWithShadowSdf*[R](
                   mode = sdfModeDropShadow)
       # shadowCache[shadowKey] = shadowImg
       echo "drawing shadow: ", innerShadow, " sz:", rect.w, "x", rect.h, " total:", cbs.totalSize, " inner:", cbs.inner, " padding:", cbs.padding, " side:", cbs.sideSize, " blur: ", shadowBlur, " spread: ", shadowSpread, " maxr:", maxRadius
-      # shadowImg.writeFile("tests/renderer-shadowImage-" & $innerShadow & "-maxr" & $maxRadius & "-totalsz" & $cbs.totalSize & "-sidesz" & $cbs.sideSize & "-blur" & $shadowBlur & "-spread" & $shadowSpread & ".png")
+      shadowImg.writeFile("tests/renderer-shadowImage-" & $innerShadow & "-maxr" & $maxRadius & "-totalsz" & $cbs.totalSize & "-sidesz" & $cbs.sideSize & "-blur" & $shadowBlur & "-spread" & $shadowSpread & ".png")
 
     # Slice it into 9-patch pieces
     let patches = sliceToNinePatch(shadowImg)
