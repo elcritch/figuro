129788110e19b3680c6b64524d2254346e7648e5
Jaremy Creechley <creechley@gmail.com>
2025-06-10 23:53:46 -0600

    update sdfy

--- src/figuro/renderer/utils/drawshadows.nim
+++ src/figuro/renderer/utils/drawshadows.nim
@@ -224,7 +224,7 @@ proc fillRoundedRectWithShadowSdf*[R](
                   spread = shadowSpread,
                   mode = sdfModeDropShadow)
       # shadowCache[shadowKey] = shadowImg
-      echo "drawing shadow: ", innerShadow, " sz:", rect.w, "x", rect.h, " total:", cbs.totalSize, " inner:", cbs.inner, " padding:", cbs.padding, " side:", cbs.sideSize, " blur: ", shadowBlur, " spread: ", shadowSpread, " maxr:", maxRadius
+      # echo "drawing shadow: ", innerShadow, " sz:", rect.w, "x", rect.h, " total:", cbs.totalSize, " inner:", cbs.inner, " padding:", cbs.padding, " side:", cbs.sideSize, " blur: ", shadowBlur, " spread: ", shadowSpread, " maxr:", maxRadius
       shadowImg.writeFile("tests/renderer-shadowImage-" & $innerShadow & "-maxr" & $maxRadius & "-totalsz" & $cbs.totalSize & "-sidesz" & $cbs.sideSize & "-blur" & $shadowBlur & "-spread" & $shadowSpread & "-rTL" & $radii[dcTopLeft] & "-rTR" & $radii[dcTopRight] & "-rBL" & $radii[dcBottomLeft] & "-rBR" & $radii[dcBottomRight] & ".png")
 
     # Slice it into 9-patch pieces
