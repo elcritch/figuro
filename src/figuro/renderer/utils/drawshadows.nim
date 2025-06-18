5906ddc9bdb86c7d66093a96da763a91587081f8
Jaremy Creechley <creechley@gmail.com>
2025-06-18 00:01:54 -0600

    shadow patch21 setup

--- src/figuro/renderer/utils/drawshadows.nim
+++ src/figuro/renderer/utils/drawshadows.nim
@@ -192,7 +192,7 @@ proc fillRoundedRectWithShadowSdf*[R](
       let sideDim = if sides[corner] in [dTop, dBottom]: w else: h
       let sideAdj = (cbs.maxRadius.float32 - cornerCbs[corner].inner.float32)
       let prevSideAdj = (cbs.maxRadius.float32 - cornerCbs[prevCorner[corner]].inner.float32)
-      let sideSize = vec2(paddingOffset, sideDim - 2*cbs.maxRadius.float32 + sideAdj + prevSideAdj)
+      let sideSize = vec2(paddingOffset / 2, sideDim - 2*cbs.maxRadius.float32 + sideAdj + prevSideAdj)
       ctx.drawImageAdj(sideHashes[sides[corner]], vec2(0, cornerCbs[corner].sideSize.float32), shadowColor, sideSize)
       ctx.restoreTransform()
 
