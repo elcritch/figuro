81383e2f329e87431d4b03c00b384555c06fa6f8
Jaremy Creechley <creechley@gmail.com>
2025-06-20 02:55:37 -0600

    fix inner

--- src/figuro/renderer/utils/drawshadows.nim
+++ src/figuro/renderer/utils/drawshadows.nim
@@ -152,7 +152,7 @@ proc fillRoundedRectWithShadowSdf*[R](
       darkGrey = rgba(50, 50, 50, 255).to(Color)
       black = rgba(0, 0, 0, 255).to(Color)
 
-      angles = [dcTopLeft: 0.0, dcTopRight: -Pi/2 + epsilon(float32), dcBottomLeft: Pi/2 + epsilon(float32), dcBottomRight: -Pi]
+      angles = [dcTopLeft: 0.0, dcTopRight: -Pi/2 - epsilon(float32), dcBottomLeft: Pi/2 + epsilon(float32), dcBottomRight: -Pi]
 
     let sides = [dcTopLeft: dLeft, dcTopRight: dTop, dcBottomLeft: dBottom, dcBottomRight: dRight]
     let prevCorner = [dcTopLeft: dcBottomLeft, dcTopRight: dcTopLeft, dcBottomLeft: dcBottomRight, dcBottomRight: dcTopRight]
